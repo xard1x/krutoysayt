c9d3a4272acd180b213d6c4f9676de6ebeafed29
xard1x <akuharenko21@gmail.com>
2025-01-23 08:49:01 +0300

    a1

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const b = 'кто прочитал тот какашка'
-const a = 'владка пидар';
+const b = 'кто прочитал тот какашка';
+console.log(b);
+let result = prompt('как тебя зовут');
+console.log('Тебя зовут', result)
+let a = confirm('Ты пидор?')
 alert(a)
-console.log(b)
