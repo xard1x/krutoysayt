adbe19087ab175c0461c778d3fc36af4896a5a00
xard1x <akuharenko21@gmail.com>
2025-01-24 09:42:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const b = 'кто прочитал тот какашка';
 console.log(b);
-let result = prompt('как тебя зовут');
-console.log('Тебя зовут', result)
-let a = confirm('Ты пидор?')
-alert(a)
+let result = prompt('ты пидор?');
+if (result === 'да'){
+    alert('ты маладец')
+} else {
+    alert('фу боже')
+}
+
