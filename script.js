19ef7c890d9024359f0bd582f6b007e1f0795238
xard1x <akuharenko21@gmail.com>
2025-01-25 06:43:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ console.log(b);
 let result = prompt('ты пидор?');
 if (result === 'да'){
     alert('ты маладец')
+} else if (result === 'не знаю') {
+    alert('ну ну')
 } else {
     alert('фу боже')
 }
