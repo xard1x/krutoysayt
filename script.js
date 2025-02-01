54bfd054dd97b4d29707ffd1a190577ee4a4f65e
xard1x <akuharenko21@gmail.com>
2025-02-01 12:20:42 +0300

    2.2

--- script.js
+++ script.js
@@ -14,23 +14,57 @@ if (result === 'да'){
 function da() {
     document.getElementById('vop').innerHTML= 'правильно, он тот еще пидар';
     maladec = maladec + 1;
+    const d3 = document.getElementById('a1')
+    const d4 = document.getElementById('a2')
+    d3.remove()
+    d4.remove()
+    if (maladec >= 3) {
+        alert('ты сигма крипер прям - вот твой код 159')
+    } else if (maladec === 2) {
+        alert('давай подожми я верю чтоты будешь сигма крипером - вот твой код 145')
+    } else {
+        alert('капиц ты лох - вот твой код 123')
+    }
 }
 function net() {
     document.getElementById('vop').innerHTML= 'ты что долбоеб написано же что он пидар';
     maladec = maladec - 1;
+    const d3 = document.getElementById('a1')
+    const d4 = document.getElementById('a2')
+    d4.remove()
+    d3.remove()
+    if (maladec >= 3) {
+        alert('ты сигма крипер прям - вот твой код 159')
+    } else if (maladec === 2) {
+        alert('давай подожми я верю чтоты будешь сигма крипером - вот твой код 145')
+    } else {
+        alert('капиц ты лох - вот твой код 123')
+    }
 }
 function da1() {
         document.getElementById('vop1').innerHTML= 'красава';
         maladec = maladec + 1;
+        const d2 = document.getElementById('a4')
+        const d1 = document.getElementById('a3')
+        d1.remove()
+        d2.remove()
 }
 function net1() {
     document.getElementById('vop1').innerHTML= 'я тебя запомнил';
     maladec = maladec - 1;
+    const d2 = document.getElementById('a4')
+    const d1 = document.getElementById('a3')
+    d2.remove()
+    d1.remove()
 }
 function dar1(){
      el = document.querySelector('body')
      el.style.background = 'black'
      el.style.color = 'white'
+     fo = document.getElementById('fon')
+    fo.style.color = 'white'
+    fo1 = document.getElementById('fon1')
+    fo1.style.color = 'white'
 }
 function whi1(){
     el = document.querySelector('body')
--- script2.js
+++ script2.js
@@ -1,7 +1,13 @@
 console.log('кашка какашка')
 function parol() {
-    p = document.getElementById('in1').value
+    const p = document.getElementById('in1').value
     console.log(p)
-    p12 = document.getElementById('in12')
-    p12.innerHTML = p + ' прикольный пароль'
+    const p12 = document.getElementById('in12')
+    p12.innerHTML = p + ' - прикольный пароль'
+}
+function tesamie() {
+    const p2 = document.getElementById('in2').value
+    console.log(p2)
+    const p22 = document.getElementById('in22')
+    p22.innerHTML = 'я запомнил '
 }
