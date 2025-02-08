456186bf8aa7442730c560fee1e2b4561fb1c2f6
xard1x <akuharenko21@gmail.com>
2025-02-08 10:09:32 +0500

    3.2.1

--- site3/script3.js
+++ site3/script3.js
@@ -1,6 +1,5 @@
-function list1() {
+function list1() { // добавление в список по кнопке и вводу
     const val = document.getElementById('in1').value
-    console.log(val)
     const newitem = document.createElement('li')
     newitem.textContent = val
     document.querySelector('#ulList').appendChild(newitem)
